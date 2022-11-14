7aa66bdbad181d1f0f7c32cbb884a4bc73f4b7b0
tayebe fazli <fazlitayebe374@gmail.com>
2022-11-14 12:06:56 +0330

    add

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,12 @@ WSGI_APPLICATION = 'blogpy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'sqldb',
+        'USER': 'Fazli',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5051',
+
     }
 }
 
@@ -116,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+project_root = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
+
+# ckeditor
+CKEDITOR_BASEPATH = '/static/ckeditor/ckeditor/'
+CKEDITOR_UPLOAD_PATH = "uploads/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
